4657a3be1a06079229a6bd13be5023a2f5ae23bd
Sourour Kraiem <Sourour.Kraiem@gmail.com>
2025-03-13 22:06:24 +0100

    Modif de nouv

--- project.py
+++ project.py
@@ -1,4 +1,9 @@
 Data=[1,3,5]
-Som=sum(Data)
-print('la somme est: ',som')
+If Data:
+    print("la somme est:",sum(Data))
+    print("le min est :",min(Data))
+    print("le max est :",max(Data))
+else:
+    print("dossier vide")
+
 
